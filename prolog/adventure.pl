1f86af3a330e33c502cbce302d8bae3476e2a298
Sebastian <sebastian.abramowski@onet.pl>
2025-04-01 00:24:36 +0200

    Moved helping rules to another file

--- prolog/adventure.pl
+++ prolog/adventure.pl
@@ -10,6 +10,7 @@
 
 :- ['place_descriptions.pl'].
 :- ['world_facts.pl'].
+:- ['help.pl'].
 
 
 /* These rules describe how to pick up an object. */
@@ -437,42 +438,6 @@ finish :-
         nl,
         write('Gra dobiegła końca. [Wpisz komendę halt., aby zakończyć]'),
         nl.
-
-
-/* This rule just writes out game instructions. */
-
-instructions :-
-    nl,
-    write('Używaj standardowej składni Prologa do wpisywania poleceń.'), nl,
-    write('Dostępne polecenia:'), nl,
-    write('start.               -- rozpocznij grę.'), nl,
-    write('go(Miejsce).         -- przejdź do wskazanego miejsca.'), nl,
-    write('look.                -- rozejrzyj się dookoła.'), nl,
-    write('take(Przedmiot).     -- podnieś przedmiot.'), nl,
-    write('drop(Przedmiot).     -- upuść przedmiot.'), nl,
-    write('talk(Postać).        -- zobacz, o co możesz zapytać daną postać.'), nl,
-    write('ask(Postać, Temat).  -- zapytaj postać o dany temat.'), nl,
-    write('shout(Postać, Temat).-- krzyknij coś na dany temat.'), nl,
-    write('notice_objects_at(Miejsce). -- pokaż, co znajduje się w danym miejscu.'), nl,
-    write('instructions.        -- pokaż tę pomoc jeszcze raz.'), nl,
-    write('wait.                -- zaczekaj chwilę.'), nl,
-    write('halt.                -- zakończ grę.'), nl,
-    nl.
-
-
-introduction :-
-    nl,
-    write('W ramach legendarnych 12 prac, które mają udowodnić wyższość Galów nad Rzymianami,'), nl,
-    write('Asterix i Obelix mają już za sobą sześć zadań.'), nl,
-    write('Pokonali dzikie bestie, przechytrzyli hipnotyzera i najedli się u kucharza olimpijskiego.'), nl, nl,
-    write('Teraz przygotowują się na kolejne wyzwanie. Tym razem jednak nie czeka ich walka na arenie,'), nl,
-    write('lecz coś znacznie gorszego... biurokracja.'), nl, nl,
-    write('Kwestor: Waszym kolejnym zadaniem będzie zdobycie zaświadczenia A38,'), nl,
-    write('które pozwoli wam uczestniczyć w następnej próbie.'), nl,
-    write('Asterix: Aaaa, chodzi o zwykłą administracyjną formalność?'), nl,
-    write('Kwestor: Tak jest, administracyjna, formalność, musicie poprosić o zaświadczenie A38'), nl, nl,
-    write('Asterix i Obelix udają się do urzędu. Wchodzą do budynku...'), nl, nl,
-    write('[Wpisz instructions. aby zobaczyć dostępne komendy]'), nl.
 
 
 /* This rule prints out instructions and tells where you are. */
